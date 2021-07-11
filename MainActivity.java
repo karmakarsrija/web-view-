ad43d1bc2d9dda14713ea725197c1cf98da39df1
karmakarsrija <karmakarsrija2@gmail.com>
2021-07-11 11:10:01 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,62 @@
+package com.com.coviddetection;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState); //create activity
+        setContentView(R.layout.activity_main); //for setting xml
+
+        webview = this.findViewById(R.id.simpleWebView); //Finds a view that was identified by the id attribute
+        webview.setWebViewClient(new CustomWebViewClient());
+        WebSettings webSetting = webview.getSettings();
+        webSetting.setJavaScriptEnabled(true);
+        webSetting.setDisplayZoomControls(true);
+        webview.getSettings().setBuiltInZoomControls(true);
+        webview.loadUrl("http://192.168.0.7/covid19-added/registration.php");
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webview.canGoBack()) {
+            webview.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private class CustomWebViewClient extends WebViewClient {
+        @Override
+       /* public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+
+
+        }*/
+
+
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (url.startsWith("tel:")) {
+                Intent intent = new Intent(Intent.ACTION_DIAL,
+                        Uri.parse(url));
+                startActivity(intent);
+            }else if(url.startsWith("http:") || url.startsWith("https:")) {
+                view.loadUrl(url);
+            }
+            return true;
+        }
+    }
+
+
+
+}
